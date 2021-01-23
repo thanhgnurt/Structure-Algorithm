574b8f8b4f50a03cb76b0fce8ee7626e3117becf
Thanh Trung <thanhgnurt@gmail.com>
2021-01-23 14:15:35 +0700

    create tree AA

---
+++ Algorithm Pratice/Trees/AA/InterfaceAA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Pratice.Trees.AA
+{
+    interface InFaceTreeAA
+    {
+
+        NodeAA root { get; set; }
+        void InsertNode(int dataNote);
+    }
+
+    interface InFaceNodeAA
+    {
+        int data { get; set; }
+        int level { get; set; }
+        NodeAA left { get; set; }
+        NodeAA right { get; set; }
+
+    }
+}
